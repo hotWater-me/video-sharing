b30836ffb4dd937c045b65d5d124f3b7fd375bb6
baoyuanlong <852604656@qq.com>
2021-05-26 00:04:05 +0800

    修改footer-bar 放入根组件

--- src/views/message/index.tsx
+++ src/views/message/index.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import FooterBar from '../../common-components/footer-bar';
 import './index.less';
 
 export interface IMessageProps {}
 
 const Message: React.FC<IMessageProps> = (props) => {
-  return (
-    <div className="message-router-wrapper">
-      <FooterBar />
-    </div>
-  );
+  return <div className="message-router-wrapper">message</div>;
 };
 
 export default Message;
